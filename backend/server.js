966c989bccee221380977f1ecdfd7e662cdc40bb
Ivan Reyes <ivan.reyes.paez@gmail.com>
2025-06-29 17:16:07 -0400

    Removed unused imports

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import userRoutes from './routes/user.routes.js'
-import bcrypt from 'bcryptjs'
-import fs from 'fs'
 import logger from './middlewares/logger.js'
 
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
-const jwt = require('jsonwebtoken')
 
 app.use(cors())
 app.use(express.json())
